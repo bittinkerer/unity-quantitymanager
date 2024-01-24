f133fd7ddd224e3f3900812db172e5ba258894fb
bittinkerer <bittinkerer@gmail.com>
2024-01-23 22:25:58 -0800

    Add QuantityRecurringUpdaterAction to continously change a QuantityManager current amount

---
+++ Runtime/Managers/QuantityRecurringUpdaterAction.cs
@@ -0,0 +1,23 @@
+﻿using Assets.Scripts.Play.Handlers;
+using Packages.Estenis.EventActions_;
+using System;
+using UnityEngine;
+using Packages.Estenis.UnityExts_;
+
+namespace Packages.com.esteny.quantitymanager.Runtime.Managers
+{
+    public class QuantityRecurringUpdaterAction : BaseGameObjectAction
+    {
+        [SerializeField] private int _change;
+        [SerializeField] private float _changeEverySeconds;
+        [SerializeField] private QuantityManager _quantityManager;
+
+        private Coroutine _updaterCoroutine;
+
+        protected override void Action(object data)
+        {
+            _updaterCoroutine = 
+                this.RunCoroutineEvery(() => _quantityManager.ChangeCurrent(_change), _changeEverySeconds);
+        }
+    }
+}
